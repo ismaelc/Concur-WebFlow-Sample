ed34849072b4c0b357d92de1da89f26bead972c3
Chris Ismael <chris.ismael@gmail.com>
2015-03-04 10:53:31 -0800

    update

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ var config = {};
 
 // Register for a Concur sandbox account at http://developer.concur.com/
 config.concur = {};
-config.concur.client_id = "JcREynzTziSSllqXWNteap";
-config.concur.client_secret = "uCNyZGKgpprMSshiu9v3VV4ukfsENCls";
+config.concur.client_id = process.env.CLIENT_ID;
+config.concur.client_secret = process.env.CLIENT_SECRET;
 
 module.exports = config;
